04207b454cb28c93b4192c15cfc1722b039d0821
FabreVictor <93644965+FabreVictor@users.noreply.github.com>
2022-02-06 16:39:57 +0100

    Add files via upload

---
+++ GD1A_SIDESCROLLER_FABREVictor/backgroundFINALE.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="backgroundFINALE " tilewidth="32" tileheight="32" tilecount="5940" columns="180">
+ <image source="../backgroundFINALE .png" width="5760" height="1080"/>
+</tileset>
---
+++ GD1A_SIDESCROLLER_FABREVictor/sprite AAA1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="sprite AAA1" tilewidth="32" tileheight="32" tilecount="100" columns="10">
+ <image source="../sprite AAA1.png" width="320" height="320"/>
+</tileset>
